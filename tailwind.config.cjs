b68965a6fedcdcb6bf31a2538b195d340fd3fe6d
Víctor José Gallego <victorjose.gallego@usc.es>
2022-10-20 18:59:04 +0200

    Add background pattern

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,24 @@
+const colors = require("tailwindcss/colors")
+
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        display: ["Silkscreen", "cursive"],
-      },
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                display: ["Silkscreen", "cursive"],
+            },
+        },
     },
-  },
-  plugins: [require('@tailwindcss/forms')],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require("tailwind-heropatterns")({
+                patterns: ["random-shapes"],
+                colors: { default: colors.slate[900], },
+                opacity: { default: "0.1" }
+            }
+        )
+    ]
 }
